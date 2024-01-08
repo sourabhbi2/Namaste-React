a434801b1f6cfaa85dd6f19cc879d3c8134da83b
sourabhbi2 <sourabhkumar1109@gmail.com>
2024-01-08 19:06:56 +0530

    named export

---
+++ Ep-04/utils/constants.js
@@ -0,0 +1,4 @@
+export const CDN_img_logo = "https://cdn6.f-cdn.com/contestentries/1318421/28372416/5aedd3a7adb01_thumb900.jpg";
+
+export const CDN_logo_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
