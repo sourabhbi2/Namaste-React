71fd4e8a9176bc9fc29300788525c3c3cf4f2421
sourabhbi2 <sourabhkumar1109@gmail.com>
2024-01-08 21:19:31 +0530

    useState

--- Ep-04/src/components/Body.js
+++ Ep-04/src/components/Body.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RestroCard from './RestroCard';
 import restList from '../utils/mockData';
 
@@ -6,13 +6,21 @@ import restList from '../utils/mockData';
 
 
 const Body = () =>{
+
+    const [restaurantList, setRestaurantList] = useState(restList)
+
     return(
         <div className='body'>
             <div className='filter'>
-                <button className='filter-btn'>Top Rated Restaurant</button>
+                <button className='filter-btn' 
+                onClick={() => {
+                    const filteredList = restaurantList.filter( (res) => res.data.avgRating > 4);
+                    setRestaurantList(filteredList);
+                }}
+                >Top Rated Restaurant</button>
                 </div>
             <div className='res-container'>
-               {restList.map((rest) =>(
+               {restaurantList.map((rest) =>(
 
                 <RestroCard key={rest.data.id} restData={rest} />
                ))} 
