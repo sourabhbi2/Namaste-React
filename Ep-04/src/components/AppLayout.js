2e0cb26c4fb4dd1d5102125a696cf2714d5ce483
sourabhbi2 <sourabhkumar1109@gmail.com>
2024-01-09 14:37:37 +0530

    Beautify

--- Ep-04/src/components/AppLayout.js
+++ Ep-04/src/components/AppLayout.js
@@ -11,23 +11,8 @@ import Body from './Body';
 }  we can add css like this */
 
 
-/* HW - config driven api ?
+// HW - config driven api ?
 
-* Two types of Export/Import :-
-  1} Default export/import :-
-      * export default Component;
-      * import Component from "path"
-   
-  2) Named Export/Import
-      * export const Component;
-      * import {Component} from "path";  
-  
-  Note:- it is important to know the use of {} while importing, because it use for named component.
-
-  3) Can we use both named export and export default component in same file?
-    ans- yes, we can use it as not more than one default export, but it can be have as many as named export as you need. 
-
-*/
 
 const AppLayout = () => {
   return (
