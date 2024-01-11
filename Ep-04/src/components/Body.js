7c4c3ff096eeac638225200712ff5655c04f635e
sourabhbi2 <sourabhkumar1109@gmail.com>
2024-01-11 19:51:01 +0530

    add: searchbar

--- Ep-04/src/components/Body.js
+++ Ep-04/src/components/Body.js
@@ -1,46 +1,78 @@
 import React, { useState, useEffect } from 'react'
 import RestroCard from './RestroCard';
 import restList from '../utils/mockData';
+import Shimmer from './Shimmer';
 
 
 
 
-const Body = () =>{
-const [restaurantList, setRestaurantList] = useState(restList);
+const Body = () => {
 
-// const [restaurantList, setRestaurantList] = useState([]);
+    const [restaurantList, setRestaurantList] = useState(restList);
+    const [filteredRestaurantList, setFilteredRest] = useState(restList);
+    const [searchText, setSearchTextBar] = useState('')
 
-useEffect(() => {
-    fetchData();
-}, []);
+    // const [restaurantList, setRestaurantList] = useState([]);
 
-const fetchData = async () => {
-    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.6215229&lng=85.1213044&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    // whenever state variables update, react triggers a reconciliation cycle(re-renders the component).
 
-    const json = data.json();
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-    console.log(json);
-    // setRestaurantList(json?.data?.cards[13])
-}
+    const fetchData = async () => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.6215229&lng=85.1213044&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+
+        const json = data.json();
+
+        console.log(json?.data?.cards);
+
+        // Optional Chaining 
+        // setRestaurantList(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
 
 
-    return(
+    }
+//conditional rendering
+
+ /* if(restaurantList.length ===0){
+    return <Shimmer />
+ } */
+
+     /* restaurantList.length === 0 ? (
+    <Shimmer /> 
+    ) : */ return (
         <div className='body'>
             <div className='filter'>
-                <button className='filter-btn' 
-                onClick={() => {
-                    const filteredList = restaurantList.filter( (res) => res.data.avgRating > 4);
-                    setRestaurantList(filteredList);
-                }}
-                >Top Rated Restaurant</button>
-                </div>
-            <div className='res-container'>
-               {restaurantList.map((rest) =>(
+                <div className='search'>
+                    <input type='text' className='search-box' value={searchText} onChange={(e) => {
+                        setSearchTextBar(e.target.value)
+                    }} />
+                    <button onClick={() => {
+                        // Filter the restaurant cards and update the UI
 
-                <RestroCard key={rest.data.id} restData={rest} />
-               ))} 
-                
-                              
+                        const filteredRestList = restaurantList.filter((res) => res.data.name.toLowerCase().includes(searchText.toLowerCase()) );
+
+                         setFilteredRest(filteredRestList);
+                        console.log(filteredRestList);
+                        
+                    }}>
+                    Search
+                    </button>
+                </div>
+                <button className='filter-btn'
+                    onClick={() => {
+                        const filteredRating = restaurantList.filter((res) => res.data.avgRating > 4);
+                        setRestaurantList(filteredRating);
+                    }}
+                >Top Rated Restaurant</button>
+            </div>
+            <div className='res-container'>
+                {filteredRestaurantList.map((rest) => (
+
+                    <RestroCard key={rest.data.id} restData={rest} />
+                ))}
+
+
             </div>
         </div>
     )
