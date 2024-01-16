7c7da5f05c815cde9c1ff01ca7a589f9e9fcef4e
sourabhbi2 <sourabhkumar1109@gmail.com>
2024-01-16 18:06:46 +0530

    add: itemAPI

--- Ep-04/src/utils/constants.js
+++ Ep-04/src/utils/constants.js
@@ -2,3 +2,4 @@ export const CDN_img_logo = "https://cdn6.f-cdn.com/contestentries/1318421/28372
 
 export const CDN_logo_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
+export const itemAPI = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.6215229&lng=85.1213044&restaurant="
