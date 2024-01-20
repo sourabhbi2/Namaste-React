a6adb610dd98fa221b580b16a82ad19b0155d7bd
sourabhbi2 <sourabhkumar1109@gmail.com>
2024-01-20 14:37:17 +0530

    Add: User() and UserClass component

--- Ep-04/src/components/About.js
+++ Ep-04/src/components/About.js
@@ -1,9 +1,22 @@
 import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
 
 const About = () => {
     return (
         <div>
-            <h1>This is food app.</h1>
+            <h1>About Us</h1>
+            <h2>This is food app.</h2>
+            <User 
+            name= {"Sourabh Kumar (functional component)"}
+            location={'Patna functional'}
+            contact={'sourabh@gmail.com ()'}
+              />
+            <UserClass 
+            name={'Sourabh Kumar (class component)'} 
+            location={'Patna classs'} 
+            contact={'sourabh@gmail.com class'}
+            />
         </div>
     )
 };
